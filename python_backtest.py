e4fce2c210557cfb1e82984dcdfb0afe8a06c43f
SimplifytradingAI <126475797+SimplifytradingAI@users.noreply.github.com>
2023-09-30 18:05:45 +0200

    Create python_backtest.py
    Python backtesting added

---
+++ python_backtest.py
@@ -0,0 +1,17 @@
+import requests
+
+
+url = "http://api.simplifytrading.ai/backtest"
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer <YOUR API KEY>'
+}
+
+json = {
+    "ModelName": "<AI MODEL NAME>",
+    "TradeType": "LONG",
+    "EntryTime": "07/06/2023 10:00:00 +00:00"
+}
+response = requests.post(url, json=json, headers=headers)
+
+print(response.json())
